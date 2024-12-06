ad766960268226772e968219aee06a27a1f6e686
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-06 09:02:53 +0700

    Membuat route untuk get admin dan add user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/admin', [UserController::class, 'index'])->name('user.index');
+
+// add user
+Route::group(['prefix' => 'admin/user'], function () {
+    Route::get('/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/add', [UserController::class, 'store'])->name('user.store');
 });
