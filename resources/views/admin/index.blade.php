d9cc3fb4cb2046ddd458bf80ec245e83724d9b36
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-06 10:26:41 +0700

    Menambah fungsi button edit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -47,7 +47,7 @@
                             </td>
 
                             <td>
-                                <a href="" class="btn btn-primary"> Edit</a>
+                                <a href="{{ route('user.edit', ['id' => $row->id]) }}" class="btn btn-primary"> Edit</a>
                                 <button class="btn btn-danger">Delete</button>
                             </td>
                         </tr>
