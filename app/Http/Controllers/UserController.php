0e5a4e9956cb93af7b501e32ebb1aa761efef296
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-06 09:01:33 +0700

    Membuat userController untuk index, create, store image

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::latest('id')->get();
+        return view('admin.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $title = "Add New User";
+        return view('admin.add_edit_user', compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'photo' => 'mimes:png,jpeg,jpg|max:2048',
+            ]
+        );
+
+        $filePath = public_path('upload');
+        $insert = new User();
+        $insert->name = $request->name;
+        $insert->email = $request->email;
+        $insert->password = bcrypt('password');
+
+        // if ($request->hasfile('photo')) {
+        //     $file = $request->file('photo');
+        //     // $file_name = time() . $file->getClientOriginalName();
+        //     $file_name = time() . '_' . str_replace(' ', '_', $file->getClientOriginalName());
+
+
+        //     $file->move($filePath, $file_name);
+        //     $insert->photo = $file_name;
+        // }
+
+        if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
+            $file = $request->file('photo');
+            $file_name = time() . '_' . str_replace(' ', '_', $file->getClientOriginalName());
+            $file->move(public_path('upload'), $file_name);
+            $insert->photo = $file_name;
+        }
+
+
+        $result = $insert->save();
+        Session::flash('success', 'User registered successfully');
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
